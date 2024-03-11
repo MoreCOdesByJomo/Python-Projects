af9bb1d902f3d0a832b7533786da42f6016345bb
myname <myemail>
2024-03-11 13:43:53 +0300

    second commit for python project

---
+++ im_afraid_of_snakes.py
@@ -0,0 +1,17 @@
+#single line comment
+"""
+multi line comment
+bluh bluh bluh
+bluh bluh bluh
+"""
+
+#variables
+student_name = "Jomo" #string
+student_age =17 #integer
+student_fees =100.0 #float
+is_male=True #boolean
+
+#the above lack prints
+
+x=y=z=10 #assigning one value to multiple variales
+x,y,z=5,10,11 #assigning multiple values to multiple variables
